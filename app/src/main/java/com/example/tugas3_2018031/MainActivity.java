c85eec87745245e8e9d6d0e7b146b0ccc502ca02
Najib Zulfan <najibzulfan@gmail.com>
2022-11-06 20:45:30 +0700

    (+)Tugas 5 Recycler View

--- app/src/main/java/com/example/tugas3_2018031/MainActivity.java
+++ app/src/main/java/com/example/tugas3_2018031/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.tugas3_2018031;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recylerView;
+
+    String s1[], s2[],s3[];
+    int images[] =
+            {R.drawable.bahan1,R.drawable.bahan2,R.drawable.bahan3,R.drawable.bahan4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recylerView = findViewById(R.id.rvDiary);
+        s1 = getResources().getStringArray(R.array.judul);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        DiaryAdapter appAdapter = new DiaryAdapter(s1,s2,s3,images, this);
+        recylerView.setAdapter(appAdapter);
+        recylerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
     }
 }
